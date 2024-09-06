58316b909bb0487f56ef4086a7a66e8110a9fc88
jonat <jonathanmtsz@outlook.com>
2024-09-05 21:52:45 -0300

    Initial commit

---
+++ src/main/java/com/agasalha/PetTrackAPI/PetTrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.agasalha.PetTrackAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetTrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetTrackApiApplication.class, args);
+	}
+
+}
