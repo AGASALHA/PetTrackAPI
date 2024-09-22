a4fea2cd965153c9d4b1de1b418702b76c487185
jonat <jonathanmtsz@outlook.com>
2024-09-22 14:31:00 -0300

    added generation of QrCode based on UUID, generation of img based on QrCode, saving of qrcode_img on database in blob(binary) format, return of blob encoded on base64 on save.

--- src/main/java/com/agasalha/PetTrackAPI/domain/services/qrcode_services/QRCodeServiceInterface.java
+++ src/main/java/com/agasalha/PetTrackAPI/domain/services/qrcode_services/QRCodeServiceInterface.java
@@ -2,8 +2,15 @@ package com.agasalha.PetTrackAPI.domain.services.qrcode_services;
 
 import com.agasalha.PetTrackAPI.domain.dtos.qrcode.request.QRCodeRequestDTO;
 import com.agasalha.PetTrackAPI.domain.dtos.qrcode.response.QRCodeResponseDTO;
+import io.nayuki.qrcodegen.QrCode;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public interface QRCodeServiceInterface {
-    QRCodeResponseDTO save(QRCodeRequestDTO qrCodeRequestDTO);
+    QRCodeResponseDTO save(QRCodeRequestDTO qrCodeRequestDTO) throws IOException;
     QRCodeResponseDTO get(Long id);
+    String generateUUID();
+    byte[] generateBlobFromImg(String uuid) throws IOException;
+    BufferedImage generateImgFromQrCode(QrCode qr, int scale, int border, int lightColor, int darkColor);
 }
